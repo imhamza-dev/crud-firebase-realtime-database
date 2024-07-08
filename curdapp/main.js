98ac2187d6fe1fd791d5ead3a79d1409adb333e4
Mr Hamza <imhamza.dev@gmail.com>
2024-07-08 12:27:20 +0500

    complete-crud-app-with-firebase-and-javaScript

--- curdapp/main.js
+++ curdapp/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 import { initializeApp } from 'firebase/app';
-import { getDatabase, set, ref } from 'firebase/database';
+import { getDatabase, set, ref, get, remove } from 'firebase/database';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDarWHUzlTBfaWoy0j18S0waQUOZpkuflE',
@@ -24,6 +24,7 @@ const saveData = () => {
 
 	if (!name || !email) {
 		notify.innerText = 'Plz fill form';
+		return;
 	}
 
 	const userId = Date.now();
@@ -31,18 +32,106 @@ const saveData = () => {
 	set(ref(db, 'users/' + userId), {
 		name: name,
 		email: email,
-	});
-
-	notify.innerText = 'data saved';
-	documnet.querySelector('#name').value = '';
-	documnet.querySelector('#email').value = '';
+	})
+		.then(() => {
+			notify.innerText = 'Data saved';
+			document.querySelector('#name').value = '';
+			document.querySelector('#email').value = '';
+		})
+		.catch((error) => {
+			notify.innerText = `Error: ${error.message}`;
+		});
 };
 
 const saveDataBtn = document.querySelector('#save');
-
 saveDataBtn.addEventListener('click', saveData);
 
 /* 
   get data 
+*/
 
-	*/
+const getData = () => {
+	const userRef = ref(db, 'users/');
+	get(userRef).then((snapshot) => {
+		const data = snapshot.val();
+		const table = document.querySelector('table');
+		let html = '';
+		if (data) {
+			Object.keys(data).forEach((key) => {
+				const { name, email } = data[key];
+
+				html += `
+				 <tr>
+					<td>${name}</td>
+					<td>${email}</td>
+					<td> <button onclick="editUser('${key}')">Edit</button> </td>
+					<td> <button class='del' onclick="deleteUser('${key}')">Delete</button> </td>
+				 </tr>
+				 `;
+			});
+		} else {
+			html = '<tr><td colspan="4">No data available</td></tr>';
+		}
+
+		table.innerHTML = html;
+	});
+};
+
+const editUser = (key) => {
+	const userRef = ref(db, `users/${key}`);
+	get(userRef).then((item) => {
+		const name = item.val().name;
+		const email = item.val().email;
+
+		document.querySelector('#name').value = name;
+		document.querySelector('#email').value = email;
+		document.querySelector('.update_btn').classList.add('show');
+		document.querySelector('.save_btn').classList.add('hide');
+
+		const updateForm = () => {
+			const name = document.querySelector('#name').value;
+			const email = document.querySelector('#email').value;
+
+			if (name && email) {
+				set(userRef, {
+					name: name,
+					email: email,
+				}).then(() => {
+					notify.innerText = 'Data updated';
+					getData();
+
+					document.querySelector('.update_btn').classList.remove('show');
+					document.querySelector('.save_btn').classList.remove('hide');
+
+					// Clear inputs
+					document.querySelector('#name').value = '';
+					document.querySelector('#email').value = '';
+				});
+			} else {
+				notify.innerText = 'Plz add data';
+			}
+		};
+
+		const updateFormBtn = document.querySelector('#update_form');
+		updateFormBtn.removeEventListener('click', updateForm);
+		updateFormBtn.addEventListener('click', updateForm);
+	});
+};
+
+const deleteUser = (key) => {
+	const userRef = ref(db, `users/${key}`);
+	remove(userRef).then(() => {
+		notify.innerText = 'Data deleted!';
+		getData();
+	});
+};
+
+// Load data initially
+getData();
+
+/*
+Get data end
+*/
+
+window.deleteUser = deleteUser;
+window.editUser = editUser;
